2dbe9458e0559e2a3348fa0336c373183985e68c
MattJasonLu <1132464757@qq.com>
2018-06-21 10:55:38 +0800

    问卷页面2基本逻辑及页面数据绑定完成

--- src/com/jdlink/controller/QuestionnaireController.java
+++ src/com/jdlink/controller/QuestionnaireController.java
@@ -110,7 +110,21 @@ public class QuestionnaireController {
     @ResponseBody
     public String savePage2Info(@RequestBody Questionnaire questionnaire) {
         JSONObject res = new JSONObject();
-
+        try {
+            // 更新原材料的信息
+            QuestionnaireController.questionnaire.setRawWastesList(questionnaire.getRawWastesList());
+            // 更新特别关注物质的信息
+            QuestionnaireController.questionnaire.setWasteInclusionTypeList(questionnaire.getWasteInclusionTypeList());
+            // 更新工艺流程的信息
+            QuestionnaireController.questionnaire.setWasteProcessList(questionnaire.getWasteProcessList());
+            res.put("status", "success");
+            res.put("message", "页面2数据保存成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            res.put("status", "fail");
+            res.put("message", "页面2数据保存失败");
+            res.put("exception", e.getMessage());
+        }
         return res.toString();
     }
 
