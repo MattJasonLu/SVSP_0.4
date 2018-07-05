0b3f446c49d1a75bacfa81b6b07f04481414dd01
MattJasonLu <1132464757@qq.com>
2018-07-05 17:30:37 +0800

    成本管理初步部署

---
+++ src/com/jdlink/service/impl/CostServiceImpl.java
@@ -0,0 +1,43 @@
+package com.jdlink.service.impl;
+
+import com.jdlink.domain.Cost;
+import com.jdlink.mapper.CostMapper;
+import com.jdlink.service.CostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by matt on 2018/7/5.
+ */
+@Service
+public class CostServiceImpl implements CostService {
+    @Autowired
+    CostMapper costMapper;
+
+    @Override
+    public void add(Cost cost) {
+        costMapper.add(cost);
+    }
+
+    @Override
+    public void update(Cost cost) {
+        costMapper.update(cost);
+    }
+
+    @Override
+    public List<Cost> list() {
+        return costMapper.list();
+    }
+
+    @Override
+    public int count() {
+        return costMapper.count();
+    }
+
+    @Override
+    public Cost getById(String costId) {
+        return costMapper.getById(costId);
+    }
+}
