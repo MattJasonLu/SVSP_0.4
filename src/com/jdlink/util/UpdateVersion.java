0bf2669283d18c609b509ff96b3f08b1656c5b8b
LeonLoveAdd <18114853285@163.com>
2018-07-16 10:44:55 +0800

    危废数据库接口2

--- src/com/jdlink/util/UpdateVersion.java
+++ src/com/jdlink/util/UpdateVersion.java
@@ -2,14 +2,21 @@ package com.jdlink.util;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.text.DecimalFormat;
 
+/**
+ * 版本更新工具类
+ * @return newVersionID 版本号
+ */
 public class UpdateVersion {
-    public String updateVersionID(String versionID){
+    public static String updateVersionID(String versionID){
         String regEx="[^0-9.]";
         Pattern p = Pattern.compile(regEx);//正则表达式
         Matcher m = p.matcher(versionID);
         Double t=Double.parseDouble(m.replaceAll("").trim());
-        String newVersionId="V"+String.valueOf(t+0.1);
+        DecimalFormat df=new DecimalFormat("#.0");
+        Double n=Double.parseDouble(df.format(t));
+        String newVersionId="V"+String.valueOf(n+0.1);
         return newVersionId;
     }
 }
