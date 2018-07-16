cb17299317032c1a88c3b22ee6549c43207aa6c3
LeonLoveAdd <18114853285@163.com>
2018-07-16 11:37:43 +0800

    危废数据库接口3

--- src/com/jdlink/util/UpdateVersion.java
+++ src/com/jdlink/util/UpdateVersion.java
@@ -1,5 +1,7 @@
 package com.jdlink.util;
 
+import org.junit.Test;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.text.DecimalFormat;
@@ -9,6 +11,9 @@ import java.text.DecimalFormat;
  * @return newVersionID 版本号
  */
 public class UpdateVersion {
+    public static void  main(String[] args){
+        System.out.println(UpdateVersion.updateVersionID("1.9"));
+    }
     public static String updateVersionID(String versionID){
         String regEx="[^0-9.]";
         Pattern p = Pattern.compile(regEx);//正则表达式
@@ -16,7 +21,7 @@ public class UpdateVersion {
         Double t=Double.parseDouble(m.replaceAll("").trim());
         DecimalFormat df=new DecimalFormat("#.0");
         Double n=Double.parseDouble(df.format(t));
-        String newVersionId="V"+String.valueOf(n+0.1);
+        String newVersionId="V"+df.format(n+0.1);
         return newVersionId;
     }
 }
