97f55d60f6758daef8d340ed49ccf0c78ed4a167
JackYang950618 <1169267380@qq.com>
2018-07-17 17:12:36 +0800

    模板列表查询框

--- src/com/jdlink/mapper/ContractMapper.java
+++ src/com/jdlink/mapper/ContractMapper.java
@@ -1,6 +1,7 @@
 package com.jdlink.mapper;
 
 import com.jdlink.domain.Contract;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -33,4 +34,7 @@ public interface ContractMapper {
     void approval(String contractId);
     List<String> modelName(String key);
     void back(String contractId);
+    @Update("update t_contract set opinion=#{1} where contractId=#{0}")
+    void opinion(String contractId,String opinion);
+
 }
--- src/com/jdlink/service/impl/ContractServiceImpl.java
+++ src/com/jdlink/service/impl/ContractServiceImpl.java
@@ -3,6 +3,7 @@ package com.jdlink.service.impl;
 import com.jdlink.domain.Contract;
 import com.jdlink.mapper.ContractMapper;
 import com.jdlink.service.ContractService;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -134,4 +135,12 @@ contractMapper.updateFreight2(id);
     public void back(String contractId) {
         contractMapper.back(contractId);
     }
+
+    @Override
+    @Update("update t_contract set opinion=#{1} where contractId=#{0}")
+    public void opinion(String contractId, String opinion) {
+        contractMapper.opinion(contractId,opinion);
+    }
+
+
 }
