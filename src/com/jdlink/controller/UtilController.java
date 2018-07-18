c410373a7b23869e675c7e9afeccdfdbcf5cc3ab
LeonLoveAdd <18114853285@163.com>
2018-07-18 08:53:49 +0800

    导出功能-UtilController

--- src/com/jdlink/controller/UtilController.java
+++ src/com/jdlink/controller/UtilController.java
@@ -2,20 +2,33 @@ package com.jdlink.controller;
 
 import com.jdlink.util.DBUtil;
 
+import javax.servlet.http.HttpServletResponse;
 import net.sf.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @Controller
 public class UtilController {
 
     @RequestMapping("exportExcel")
     @ResponseBody
-    public String exportExcel(String name){
-        DBUtil db=new DBUtil();
-        return db.exportExcel(name);
+    public String exportExcel(String name,HttpServletResponse response) {
+        JSONObject res = new JSONObject();
+        try {
+            DBUtil db = new DBUtil();
+            db.exportExcel(name,response);//HttpServletResponse response
+            res.put("status", "success");
+            res.put("message", "导出成功");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            res.put("status", "fail");
+            res.put("message", "导出失败，请重试！");
+        }
+        return res.toString();
     }
 
     @RequestMapping("importExcel")
