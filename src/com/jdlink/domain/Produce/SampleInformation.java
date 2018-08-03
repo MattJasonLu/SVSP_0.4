a96839347b3056dabf48f15e9c39c4773edc7c8a
LeonLoveAdd <18114853285@163.com>
2018-08-03 08:33:29 +0800

    样品信息数据结构

--- src/com/jdlink/domain/Produce/SampleInformation.java
+++ src/com/jdlink/domain/Produce/SampleInformation.java
@@ -109,6 +109,12 @@ public class SampleInformation {
         this.laboratorySigner = laboratorySigner;
     }
 
+    public ApplyState getApplyState() { return applyState;
+    }
+
+    public void setApplyState(ApplyState applyState) { this.applyState = applyState;
+    }
+
     public boolean getIsPH() {
         return isPH;
     }
@@ -189,4 +195,23 @@ public class SampleInformation {
         isViscosity = viscosity;
     }
 
+    @Override
+    public String toString() {
+        return "SampleInformation{" +
+                "companyCode='" + companyCode + '\'' +
+                ", wastesCode='" + wastesCode + '\'' +
+                ", laboratorySigner='" + laboratorySigner + '\'' +
+                ", applyState=" + applyState +
+                ", isPH=" + isPH +
+                ", isAsh=" + isAsh +
+                ", isWater=" + isWater +
+                ", isHeat=" + isHeat +
+                ", isSulfur=" + isSulfur +
+                ", isChlorine=" + isChlorine +
+                ", isFluorine=" + isFluorine +
+                ", isPhosphorus=" + isPhosphorus +
+                ", isFlashPoint=" + isFlashPoint +
+                ", isViscosity=" + isViscosity +
+                '}';
+    }
 }
