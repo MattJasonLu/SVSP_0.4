260c03cb4dba0321c46a3b3e8ae8f4272a4bbc47
Leon <18114853285@163.com>
2018-08-06 15:24:11 +0800

    物料需求单页面完成

---
+++ web/js/produce/materialDemand.js
@@ -0,0 +1,426 @@
+var clientId;
+
+/**
+ * 导航问题
+ * */
+$(function(){
+    autoNav();
+});
+
+//解决底部自动导航的问题
+function autoNav(){
+    //获取内容的高度
+    var bodyHeight = $("body").height();
+    //获取底部导航的高度
+    var navHeight = $(".navbar").height();
+    //获取显示屏的高度
+    var iHeight = document.documentElement.clientHeight||document.body.clientHeight;
+    //如果内容的高度大于（窗口的高度 - 导航的高度）,z则需要添加一个div，设置其高度
+    if(bodyHeight > (iHeight - navHeight)){
+        $("body").append('<div style="height: '+navHeight+'px"></div>');
+    }
+}
+/**
+ * 获取用户的编号
+ * @param item
+ * @returns {string}
+ */
+function getClientId(e) {
+    return e.firstElementChild.innerHTML;
+}
+
+/**
+ * 显示客户的文件列表
+ * @param e
+ */
+function showClientFiles(e) {
+    var clientId = getClientId(e);
+    window.location.href = "archives.html?clientId=" + clientId;
+}
+
+var currentPage = 1;                          //当前页数
+
+/**
+ * 返回count值
+ * */
+function countValue(){
+    var mySelect=document.getElementById("count");
+    var index=mySelect.selectedIndex;
+    return mySelect.options[index].text;
+}
+/**
+ * 计算总页数
+ * */
+function totalPage() {
+    var totalRecord = 0;
+    $.ajax({
+        type: "POST",                       // 方法类型
+        url: "totalRecord",                  // url
+        async: false,                      // 同步：意思是当有返回值以后才会进行后面的js程序
+        dataType: "json",
+        success: function (result) {
+            if (result > 0) {
+                totalRecord = result;
+            } else {
+                console.log("fail: " + result);
+                totalRecord = 0;
+            }
+        },
+        error: function (result) {
+            console.log("error: " + result);
+            totalRecord = 0;
+        }
+    });
+    var count = countValue();                         // 可选
+    var total = loadPages(totalRecord, count);
+    return total;
+}
+
+/**
+ * 设置克隆页码
+ * */
+function setPageClone(result) {
+    $(".beforeClone").remove();
+    setClientList(result);
+    var total = totalPage();
+    $("#next").prev().hide();
+    var st = "共" + total + "页";
+    $("#totalPage").text(st);
+    var myArray = new Array();
+    for (var i = 0; i < total; i++) {
+        var li = $("#next").prev();
+        myArray[i] = i + 1;
+        var clonedLi = li.clone();
+        clonedLi.show();
+        clonedLi.find('a:first-child').text(myArray[i]);
+        clonedLi.find('a:first-child').click(function () {
+            var num = $(this).text();
+            switchPage(num);
+        });
+        clonedLi.addClass("beforeClone");
+        clonedLi.removeAttr("id");
+        clonedLi.insertAfter(li);
+    }
+
+}
+
+/**
+ * 点击页数跳转页面
+ * @param pageNumber 跳转页数
+ * */
+function switchPage(pageNumber) {
+    console.log("当前页：" + pageNumber);
+    if (pageNumber === 0) {                 //首页
+        pageNumber = 1;
+    }
+    if (pageNumber === -2) {
+        pageNumber = totalPage();        //尾页
+    }
+    if (pageNumber == null || pageNumber === undefined) {
+        console.log("参数为空,返回首页!");
+        pageNumber = 1;
+    }
+    $("#current").find("a").text("当前页："+pageNumber);
+    if (pageNumber === 1) {
+        $("#previous").addClass("disabled");
+        $("#firstPage").addClass("disabled");
+        $("#next").removeClass("disabled");
+        $("#endPage").removeClass("disabled");
+    }
+    if (pageNumber === totalPage()) {
+        $("#next").addClass("disabled");
+        $("#endPage").addClass("disabled");
+        $("#previous").removeClass("disabled");
+        $("#firstPage").removeClass("disabled");
+    }
+    if(pageNumber > 1){
+        $("#previous").removeClass("disabled");
+        $("#firstPage").removeClass("disabled");
+    }
+    if(pageNumber < totalPage()){
+        $("#next").removeClass("disabled");
+        $("#endPage").removeClass("disabled");
+    }
+    var page = {};
+    page.count = countValue();                        //可选
+    page.pageNumber = pageNumber;
+    currentPage = pageNumber;          //当前页面
+    //addClass("active");
+    page.start = (pageNumber - 1) * page.count;
+    $.ajax({
+        type: "POST",                       // 方法类型
+        url: "loadPageClientList",         // url
+        async: false,                      // 同步：意思是当有返回值以后才会进行后面的js程序
+        data: JSON.stringify(page),
+        dataType: "json",
+        contentType: 'application/json;charset=utf-8',
+        success: function (result) {
+            if (result !== undefined) {
+                setClientList(result);
+            } else {
+                console.log("fail: " + result);
+            }
+        },
+        error: function (result) {
+            console.log("error: " + result);
+        }
+    });
+}
+
+/**
+ * 输入页数跳转页面
+ * */
+function inputSwitchPage() {
+    var pageNumber = $("#pageNumber").val();    // 获取输入框的值
+    $("#current").find("a").text("当前页："+pageNumber);
+    if (pageNumber === null || pageNumber === undefined) {
+        window.alert("跳转页数不能为空！")
+    } else {
+        if (pageNumber === 1) {
+            $("#previous").addClass("disabled");
+            $("#firstPage").addClass("disabled");
+            $("#next").removeClass("disabled");
+            $("#endPage").removeClass("disabled");
+        }
+        if (pageNumber === totalPage()) {
+            $("#next").addClass("disabled");
+            $("#endPage").addClass("disabled");
+            $("#previous").removeClass("disabled");
+            $("#firstPage").removeClass("disabled");
+        }
+        if(pageNumber > 1){
+            $("#previous").removeClass("disabled");
+            $("#firstPage").removeClass("disabled");
+        }
+        if(pageNumber < totalPage()){
+            $("#next").removeClass("disabled");
+            $("#endPage").removeClass("disabled");
+        }
+        currentPage = pageNumber;
+        var page = {};
+        page.count = countValue();//可选
+        page.pageNumber = pageNumber;
+        page.start = (pageNumber - 1) * page.count;
+        $.ajax({
+            type: "POST",                       // 方法类型
+            url: "loadPageClientList",         // url
+            async: false,                      // 同步：意思是当有返回值以后才会进行后面的js程序
+            data: JSON.stringify(page),
+            dataType: "json",
+            contentType: 'application/json;charset=utf-8',
+            success: function (result) {
+                if (result !== undefined) {
+                    console.log(result);
+                    setClientList(result);
+                } else {
+                    console.log("fail: " + result);
+                }
+            },
+            error: function (result) {
+                console.log("error: " + result);
+            }
+        });
+
+
+    }
+}
+
+/**
+ * 分页 获取首页内容
+ * */
+function loadPageClientList() {
+    var pageNumber = 1;               // 显示首页
+    $("#current").find("a").text("当前页：1");
+    $("#previous").addClass("disabled");
+    $("#firstPage").addClass("disabled");
+    var page = {};
+    page.count = countValue();                                 // 可选
+    page.pageNumber = pageNumber;
+    page.start = (pageNumber - 1) * page.count;
+    $.ajax({
+        type: "POST",                       // 方法类型
+        url: "loadPageClientList",          // url
+        async: false,                       // 同步：意思是当有返回值以后才会进行后面的js程序
+        data: JSON.stringify(page),
+        dataType: "json",
+        contentType: 'application/json;charset=utf-8',
+        success: function (result) {
+            if (result !== undefined) {
+                console.log(result);
+                setPageClone(result);
+            } else {
+                console.log("fail: " + result);
+            }
+        },
+        error: function (result) {
+            console.log("error: " + result);
+            console.log("失败");
+        }
+    });
+}
+
+/**
+ * 计算分页总页数
+ * @param totalRecord
+ * @param count
+ * @returns {number}
+ */
+function loadPages(totalRecord, count) {
+    if (totalRecord === 0) {
+        window.alert("总记录数为0，请检查！");
+        return 0;
+    }
+    else if (totalRecord % count === 0)
+        return totalRecord / count;
+    else
+        return parseInt(totalRecord / count) + 1;
+}
+
+function setClientList(result) {
+    // 获取id为cloneTr的tr元素
+    var tr = $("#cloneTr");
+    tr.siblings().remove();
+    $.each(result, function (index, item) {
+        // 克隆tr，每次遍历都可以产生新的tr
+        var clonedTr = tr.clone();
+        clonedTr.show();
+        var _index = index;
+        // 循环遍历cloneTr的每一个td元素，并赋值
+        clonedTr.children("td").each(function (inner_index) {
+            var obj = eval(item);
+            // 根据索引为部分td赋值
+            switch (inner_index) {
+                // 企业编号
+                case (0):
+                    $(this).html(obj.clientId);
+                    break;
+                // 企业名称
+                case (1):
+                    $(this).html(obj.companyName);
+                    break;
+                // 由谁添加
+                case (2):
+                    $(this).html(obj.contactName);
+                    break;
+                // 添加时间
+                case (3):
+                    $(this).html(getNowDate());
+                    break;
+                // 编辑时间
+                case (4):
+                    $(this).html(getNowDate());
+                    break;
+                // 操作
+                case (5):
+
+                    break;
+            }
+        });
+        // 把克隆好的tr追加到原来的tr前面
+        clonedTr.removeAttr("id");
+        clonedTr.insertBefore(tr);
+    });
+    // 隐藏无数据的tr
+    tr.hide();
+}
+function loadClientList() {
+    $.ajax({
+        type: "POST",                       // 方法类型
+        url: "listClient",                  // url
+        async : false,                      // 同步：意思是当有返回值以后才会进行后面的js程序
+        dataType: "json",
+        success: function (result) {
+            if (result !== undefined) {
+                console.log(result);
+                setClientList(result);
+            } else {
+                console.log("fail: " + result);
+            }
+        },
+        error:function (result) {
+            console.log("error: " + result);
+        }
+    });
+}
+function setClientList(result) {
+    // 获取id为cloneTr的tr元素
+    var tr = $("#cloneTr");
+    tr.siblings().remove();
+    $.each(result, function (index, item) {
+        // 克隆tr，每次遍历都可以产生新的tr
+        var clonedTr = tr.clone();
+        clonedTr.show();
+        var _index = index;
+        // 循环遍历cloneTr的每一个td元素，并赋值
+        clonedTr.children("td").each(function (inner_index) {
+            var obj = eval(item);
+            // 根据索引为部分td赋值
+            switch (inner_index) {
+                // 企业编号
+                case (0):
+                    $(this).html(obj.clientId);
+                    break;
+                // 企业名称
+                case (1):
+                    $(this).html(obj.companyName);
+                    break;
+                // 由谁添加
+                case (2):
+                    $(this).html(obj.contactName);
+                    break;
+                // 添加时间
+                case (3):
+                    $(this).html(getNowDate());
+                    break;
+                // 编辑时间
+                case (4):
+                    $(this).html(getNowDate());
+                    break;
+                // 操作
+                case (5):
+
+                    break;
+            }
+        });
+        // 把克隆好的tr追加到原来的tr前面
+        clonedTr.removeAttr("id");
+        clonedTr.insertBefore(tr);
+    });
+    // 隐藏无数据的tr
+    tr.hide();
+}
+function loadClientList() {
+    $.ajax({
+        type: "POST",                       // 方法类型
+        url: "listClient",                  // url
+        async : false,                      // 同步：意思是当有返回值以后才会进行后面的js程序
+        dataType: "json",
+        success: function (result) {
+            if (result != undefined) {
+                console.log(result);
+                setClientList(result);
+            } else {
+                console.log("fail: " + result);
+            }
+        },
+        error:function (result) {
+            console.log("error: " + result);
+        }
+    });
+}
+var m = new Date();
+var month = m.getMonth();
+var y = new Date();
+var year = y.getFullYear();
+//当月的第几周
+var getMonthWeek = function (a, b, c) {
+    //a = d = 当前日期
+    //b = 6 - w = 当前周的还有几天过完(不算今天)
+    //a + b 的和在除以7 就是当天是当前月份的第几周
+    var date = new Date(a, parseInt(b) - 1, c), w = date.getDay(), d = date.getDate();
+    return Math.ceil((d + 6 - w) / 7);
+};
+$(".week").text(getMonthWeek());
+$(".month").text(month);
+$(".year").text(year);
+console.log(getMonthWeek()+month+year);
