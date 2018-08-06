1f7d96b68f25da1ab02f1a7810557c19a738287d
JackYang950618 <1169267380@qq.com>
2018-08-06 11:13:28 +0800

    基础数据阈值表 和 配伍计划表 实体类创建

---
+++ src/com/jdlink/domain/Produce/Threshold.java
@@ -0,0 +1,234 @@
+package com.jdlink.domain.Produce;
+
+import com.jdlink.domain.FormType;
+
+import java.util.Date;
+
+public class Threshold {
+    //序号
+    private String thresholdId;
+    //处理类别
+    private HandleCategory handleCategory;
+    //物质形态
+    private FormType formType;
+    //热值最大值
+    private  float calorificMax;
+    //热值最小值
+    private  float calorificMin;
+    //灰分最大值
+    private float ashMax;
+    //灰分最小值
+    private float ashMin;
+    //硫最大值
+    private  float sMax;
+    //硫最小值
+    private  float sMin;
+    //氯最大值
+    private  float clMax;
+    //氯最小值
+    private  float clMin;
+    //磷最大值
+    private  String pMax;
+    //磷最小炮值
+    private  float pMin;
+    //弗最大值
+    private float fMax;
+    //弗最小值
+    private float fMin;
+    //PH最大值
+    private  float phMax;
+    //PH最小值
+    private  float phMin;
+    //安全库存量
+    private float safety;
+    //起始日期
+    private Date beginTime;
+    //结束日期
+    private Date endTimeTime;
+
+    public String getThresholdId() {
+        return thresholdId;
+    }
+
+    public void setThresholdId(String thresholdId) {
+        this.thresholdId = thresholdId;
+    }
+
+    public HandleCategory getHandleCategory() {
+        return handleCategory;
+    }
+
+    public void setHandleCategory(HandleCategory handleCategory) {
+        this.handleCategory = handleCategory;
+    }
+
+    public FormType getFormType() {
+        return formType;
+    }
+
+    public void setFormType(FormType formType) {
+        this.formType = formType;
+    }
+
+    public float getCalorificMax() {
+        return calorificMax;
+    }
+
+    public void setCalorificMax(float calorificMax) {
+        this.calorificMax = calorificMax;
+    }
+
+    public float getCalorificMin() {
+        return calorificMin;
+    }
+
+    public void setCalorificMin(float calorificMin) {
+        this.calorificMin = calorificMin;
+    }
+
+    public float getAshMax() {
+        return ashMax;
+    }
+
+    public void setAshMax(float ashMax) {
+        this.ashMax = ashMax;
+    }
+
+    public float getAshMin() {
+        return ashMin;
+    }
+
+    public void setAshMin(float ashMin) {
+        this.ashMin = ashMin;
+    }
+
+    public float getsMax() {
+        return sMax;
+    }
+
+    public void setsMax(float sMax) {
+        this.sMax = sMax;
+    }
+
+    public float getsMin() {
+        return sMin;
+    }
+
+    public void setsMin(float sMin) {
+        this.sMin = sMin;
+    }
+
+    public float getClMax() {
+        return clMax;
+    }
+
+    public void setClMax(float clMax) {
+        this.clMax = clMax;
+    }
+
+    public float getClMin() {
+        return clMin;
+    }
+
+    public void setClMin(float clMin) {
+        this.clMin = clMin;
+    }
+
+    public String getpMax() {
+        return pMax;
+    }
+
+    public void setpMax(String pMax) {
+        this.pMax = pMax;
+    }
+
+    public float getpMin() {
+        return pMin;
+    }
+
+    public void setpMin(float pMin) {
+        this.pMin = pMin;
+    }
+
+    public float getfMax() {
+        return fMax;
+    }
+
+    public void setfMax(float fMax) {
+        this.fMax = fMax;
+    }
+
+    public float getfMin() {
+        return fMin;
+    }
+
+    public void setfMin(float fMin) {
+        this.fMin = fMin;
+    }
+
+    public float getPhMax() {
+        return phMax;
+    }
+
+    public void setPhMax(float phMax) {
+        this.phMax = phMax;
+    }
+
+    public float getPhMin() {
+        return phMin;
+    }
+
+    public void setPhMin(float phMin) {
+        this.phMin = phMin;
+    }
+
+    public float getSafety() {
+        return safety;
+    }
+
+    public void setSafety(float safety) {
+        this.safety = safety;
+    }
+
+    public Date getBeginTime() {
+        return beginTime;
+    }
+
+    public void setBeginTime(Date beginTime) {
+        this.beginTime = beginTime;
+    }
+
+    public Date getEndTimeTime() {
+        return endTimeTime;
+    }
+
+    public void setEndTimeTime(Date endTimeTime) {
+        this.endTimeTime = endTimeTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Threshold{" +
+                "thresholdId='" + thresholdId + '\'' +
+                ", handleCategory=" + handleCategory +
+                ", formType=" + formType +
+                ", calorificMax=" + calorificMax +
+                ", calorificMin=" + calorificMin +
+                ", ashMax=" + ashMax +
+                ", ashMin=" + ashMin +
+                ", sMax=" + sMax +
+                ", sMin=" + sMin +
+                ", clMax=" + clMax +
+                ", clMin=" + clMin +
+                ", pMax='" + pMax + '\'' +
+                ", pMin=" + pMin +
+                ", fMax=" + fMax +
+                ", fMin=" + fMin +
+                ", phMax=" + phMax +
+                ", phMin=" + phMin +
+                ", safety=" + safety +
+                ", beginTime=" + beginTime +
+                ", endTimeTime=" + endTimeTime +
+                '}';
+    }
+}
