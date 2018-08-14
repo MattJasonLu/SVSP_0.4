8c7f4997f210dc15a31fcdd2d820e0bfd37562ae
JackYang950618 <1169267380@qq.com>
2018-08-14 16:12:03 +0800

    物料需求导入完成

--- src/com/jdlink/controller/MaterialRequireController.java
+++ src/com/jdlink/controller/MaterialRequireController.java
@@ -1,15 +1,15 @@
 package com.jdlink.controller;
 
+import com.jdlink.domain.FormType;
 import com.jdlink.domain.MixingElement;
-import com.jdlink.domain.Produce.Compatibility;
-import com.jdlink.domain.Produce.MaterialRequire;
-import com.jdlink.domain.Produce.Parameter;
-import com.jdlink.domain.Produce.Threshold;
+import com.jdlink.domain.PackageType;
+import com.jdlink.domain.Produce.*;
 import com.jdlink.domain.Wastes;
 import com.jdlink.service.CompatibilityService;
 import com.jdlink.service.MaterialRequireService;
 import com.jdlink.service.MixingElementService;
 import com.jdlink.service.ThresholdService;
+import com.jdlink.util.ImportUtil;
 import com.jdlink.util.RandomUtil;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -258,6 +259,219 @@ try{
         }
         return  res.toString();
       }
+      /*导入功能*/
+     @RequestMapping("importMaterialRequireIdExcel")
+     @ResponseBody
+     public String  importMaterialRequireIdExcel(MultipartFile excelFile){
+         JSONObject res = new JSONObject();
+         String fileName = excelFile.getOriginalFilename();
+         Object[][] data = ImportUtil.getInstance().getExcelFileData(excelFile);
+         Calendar cal = Calendar.getInstance();
+         //获取年
+         String year=String.valueOf(cal.get(Calendar.YEAR));
+         //获取月
+         String mouth= getMouth(String.valueOf(cal.get(Calendar.MONTH)+1));
+         //序列号
+         String number = "001";
+         //物料编号设置
+         List<String> materialRequireList = materialRequireService.check();
+         int total1 = materialRequireService.total();
+         if (total1 == 0) {
+             number = "001";
+         }
+         if (total1 != 0) {
+             String s = materialRequireList.get(0);//原字符串
+             String s2 = s.substring(s.length() - 3, s.length());//最后一个3字符
+             number = getString3(String.valueOf(Integer.parseInt(s2) + 1));
+         }
+         //物料编号
+         String materialRequireId = year + mouth + number;
+         //进行数据绑定
+         try {
+             for(int i=2;i<data.length;i++){
+                 MaterialRequire materialRequire = new MaterialRequire();
+                 List<Wastes> wastesList = new ArrayList<>();
+                 List<MixingElement> parameterList = new ArrayList<>();
+                 Wastes wastes = new Wastes();//危废信息
+                 materialRequire.setMaterialRequireId(materialRequireId);
+                 int total = materialRequireService.total();
+                 //设置序号
+                 if (total == 0) {
+                     materialRequire.setId("1");
+                 } else {
+                     materialRequire.setId(String.valueOf(total + 1));
+                 }
+                 wastes.setId(RandomUtil.getRandomEightNumber());
+                 //第二列是一个枚举进行进行判断，首先是遍历第二列 处理方式
+                 if(data[i][1].toString()!="null"){
+                     HandleCategory handleCategory =(HandleCategory.getHandleCategory(data[i][1].toString()));
+                     materialRequire.setHandleCategory(handleCategory);//射入
+                 }
+                 if(data[i][1].toString()=="null")
+                     materialRequire.setHandleCategory(null);//射入
+                 //第三列是一个枚举进行进行判断，首先是遍历第二列 包装
+                 if(data[i][2].toString()!="null"){
+                     PackageType packageType=(PackageType.getPackageType(data[i][2].toString())) ;
+                     materialRequire.setPackageType(packageType);
+                 }
+                 if(data[i][2]=="null")
+                     materialRequire.setPackageType(null);
+                 //第四列是一个枚举进行进行判断，首先是遍历第二列 形态
+                 if(data[i][3].toString()!="null"){
+                     FormType formType=FormType.getFormType(data[i][3].toString());
+                     materialRequire.setFormType(formType);
+                 }
+                 if(data[i][3]=="null"){
+                     materialRequire.setFormType(null);
+                 }
+                 //第五列是周生产计划量
+                 if(data[i][4].toString()!="null"){
+                     materialRequire.setWeeklyDemand(Float.parseFloat(data[i][4].toString()));
+                 }
+                 if(data[i][4]=="null"){
+                     materialRequire.setWeeklyDemand(0);
+                 }
+                 //第六列是目前库存量
+                 if(data[i][5].toString()!="null"){
+                     materialRequire.setCurrentInventory(Float.parseFloat(data[i][5].toString()));
+                 }
+                 if(data[i][5].toString()=="null"){
+                     materialRequire.setSafety(0);
+                 }
+                 //第七列安全库存量
+                 if(data[i][6].toString()!="null"){
+                     materialRequire.setSafety(Float.parseFloat(data[i][6].toString()));
+                 }
+                 if(data[i][6].toString()=="null"){
+                     materialRequire.setSafety(0);
+                 }
+                 //第8列市场采购量
+                 if(data[i][7].toString()!="null"){
+                     materialRequire.setMarketPurchases(Float.parseFloat(data[i][7].toString()));
+                 }
+                 if(data[i][7].toString()=="null"){
+                     materialRequire.setMarketPurchases(0);
+                 }
+                 //第9列是热值最大 第10列是热值最小
+                 MixingElement parameter = new MixingElement();
+                 parameter.setParameter(Parameter.values()[3]);//设置热值
+                 if(data[i][8].toString()!="null"&&data[i][9].toString()!="null"){
+                     parameter.setMinimum(Float.parseFloat(data[i][8].toString()));
+                     parameter.setMaximum(Float.parseFloat(data[i][9].toString()));
+                 }
+                 if(data[i][8].toString()=="null"||data[i][9].toString()=="null"){
+                     parameter.setMinimum(0);
+                     parameter.setMaximum(0);
+                 }
+                 parameter.setId(RandomUtil.getRandomEightNumber());
+                 parameterList.add(parameter);
+                 //第11列是灰分最大 第12列是灰分最小
+                 MixingElement parameter1 = new MixingElement();
+                 parameter1.setParameter(Parameter.values()[4]);//设置灰分
+                 if(data[i][10].toString()!="null"&&data[i][11].toString()!="null"){
+                     parameter1.setMinimum(Float.parseFloat(data[i][10].toString()));
+                     parameter1.setMaximum(Float.parseFloat(data[i][11].toString()));
+                 }
+                 if(data[i][10].toString()=="null"||data[i][11].toString()=="null"){
+                     parameter1.setMinimum(0);
+                     parameter1.setMaximum(0);
+                 }
+                 parameter1.setId(RandomUtil.getRandomEightNumber());
+                 parameterList.add(parameter1);
+                 //第13列是水分最大 第14列是水分最小
+                 MixingElement parameter2 = new MixingElement();
+                 parameter2.setParameter(Parameter.values()[8]);
+                 if(data[i][12].toString()!="null"&&data[i][13].toString()!="null"){
+                     parameter2.setMinimum(Float.parseFloat(data[i][12].toString()));
+                     parameter2.setMaximum(Float.parseFloat(data[i][13].toString()));
+                 }
+                 if(data[i][12].toString()=="null"||data[i][13].toString()=="null"){
+                     parameter2.setMinimum(0);
+                     parameter2.setMaximum(0);
+                 }
+                 parameter2.setId(RandomUtil.getRandomEightNumber());
+                 parameterList.add(parameter2);
+                 //第15列是硫最大 第16列是硫最小
+                 MixingElement parameter3 = new MixingElement();
+                 parameter3.setParameter(Parameter.values()[10]);//设置硫
+                 if(data[i][14].toString()!="null"&&data[i][15].toString()!="null"){
+                     parameter3.setMinimum(Float.parseFloat(data[i][14].toString()));
+                     parameter3.setMaximum(Float.parseFloat(data[i][15].toString()));
+                 }
+                 if(data[i][14].toString()=="null"||data[i][15].toString()=="null"){
+                     parameter3.setMinimum(0);
+                     parameter3.setMaximum(0);
+                 }
+                 parameter3.setId(RandomUtil.getRandomEightNumber());
+                 parameterList.add(parameter3);
+                 //第17列是氯最大 第18列是氯最小
+                 MixingElement parameter4 = new MixingElement();
+                 parameter4.setParameter(Parameter.values()[11]);
+                 if(data[i][16].toString()!="null"&&data[i][17].toString()!="null"){
+                     parameter4.setMinimum(Float.parseFloat(data[i][16].toString()));
+                     parameter4.setMaximum(Float.parseFloat(data[i][17].toString()));
+                 }
+                 if(data[i][16].toString()=="null"||data[i][17].toString()=="null"){
+                     parameter4.setMinimum(0);
+                     parameter4.setMaximum(0);
+                 }
+                 parameter4.setId(RandomUtil.getRandomEightNumber());
+                 parameterList.add(parameter4);
+                 //第19列是磷最大 第20列是磷最小
+                 MixingElement parameter5 = new MixingElement();
+                 parameter5.setParameter(Parameter.values()[13]);//设置磷
+                 if(data[i][18].toString()!="null"&&data[i][19].toString()!="null"){
+                     parameter5.setMinimum(Float.parseFloat(data[i][18].toString()));
+                     parameter5.setMaximum(Float.parseFloat(data[i][19].toString()));
+                 }
+                 if(data[i][18].toString()=="null"||data[i][19].toString()=="null"){
+                     parameter5.setMinimum(0);
+                     parameter5.setMaximum(0);
+                 }
+                 parameter5.setId(RandomUtil.getRandomEightNumber());
+                 parameterList.add(parameter5);
+                 //第21列是氟最大 第22列是氟最小
+                 MixingElement parameter6 = new MixingElement();
+                 parameter6.setParameter(Parameter.values()[12]);//设置磷
+                 if(data[i][20].toString()!="null"&&data[i][21].toString()!="null"){
+                     parameter6.setMinimum(Float.parseFloat(data[i][20].toString()));
+                     parameter6.setMaximum(Float.parseFloat(data[i][21].toString()));
+                 }
+                 if(data[i][20].toString()=="null"||data[i][21].toString()=="null"){
+                     parameter6.setMinimum(0);
+                     parameter6.setMaximum(0);
+                 }
+                 parameter6.setId(RandomUtil.getRandomEightNumber());
+                 parameterList.add(parameter6);
+                 //第23列是ph最大 第24列是ph最小
+                 MixingElement parameter7 = new MixingElement();
+                 parameter7.setParameter(Parameter.values()[2]);//设置磷
+                 if(data[i][22].toString()!="null"&&data[i][23].toString()!="null"){
+                     parameter7.setMinimum(Float.parseFloat(data[i][22].toString()));
+                     parameter7.setMaximum(Float.parseFloat(data[i][23].toString()));
+                 }
+                 if(data[i][22].toString()=="null"||data[i][23].toString()=="null"){
+                     parameter7.setMinimum(0);
+                     parameter7.setMaximum(0);
+                 }
+                 parameter7.setId(RandomUtil.getRandomEightNumber());
+                 parameterList.add(parameter7);
+                 wastes.setParameterList(parameterList);
+                 wastesList.add(wastes);
+                 materialRequire.setWastesList(wastesList);
+                 materialRequireService.addMix(materialRequire);
+             }
+             res.put("status", "success");
+             res.put("message", "导入成功");
+         }
+         catch (Exception e){
+             e.printStackTrace();
+             res.put("status", "fail");
+             res.put("message", "导入失败！");
+         }
+
+         return res.toString();
+     }
     //获取三位序列号
     public static String getString3(String id){
         while (id.length()!=3){
--- web/js/produce/materialDemand.js
+++ web/js/produce/materialDemand.js
@@ -36,22 +36,44 @@ function setMaterialList(obj,n) {
     dailyProportionsTotal=0;
     //每周需求总量
     weeklyDemandTotal=0;
-    //热值总量
-    calorificTotal=0;
-    //灰分总量
-    ashTotal=0;
-    //水分总量
-    waterTotal=0;
-    //氯总量
-    clTotal=0;
-    //硫总量
-    sTotal=0;
-    //磷总量
-    pTotal=0;
-    //弗总量
-    fTotal=0;
-    //PH总量
-    phTotal=0;
+    //热值最大总量
+    calorificmaxTotal=0;
+    //热值最小总量
+    calorificminTotal=0;
+    //灰分最大总量
+    ashmaxTotal=0;
+    //灰分最小总量
+    ashminTotal=0;
+    //水分最大总量
+    watermaxTotal=0;
+    //水分最小总量
+    waterminTotal=0;
+    //氯最大总量
+    clmaxTotal=0;
+    //氯最大总量
+    clminTotal=0;
+    //硫最大总量
+    smaxTotal=0;
+    //硫最小总量
+    sminTotal=0;
+    //磷最大总量
+    pmaxTotal=0;
+    //磷最小总量
+    pminTotal=0;
+    //弗最大总量
+    fmaxTotal=0;
+    //弗最小总量
+    fminTotal=0;
+    //PH最大总量
+    phmaxTotal=0;
+    //PH最小总量
+    phminTotal=0;
+    //目前库存总量
+    currentInventoryTotal=0;
+    //安全库存总和
+    safetyTotal=0;
+    //市场采购量总和
+    marketPurchasesTotal=0;
     $.each(obj, function (index, item) {
         var data = eval(item);
         var clonedTr = tr.clone();
@@ -93,18 +115,22 @@ function setMaterialList(obj,n) {
                 // 周生产计划量(T)
                 case (4):
                     $(this).html(data.weeklyDemand);
+                    dailyProportionsTotal+=data.weeklyDemand;
                     break;
                 //目前库存量(T)
                 case (5):
                     $(this).html(data.currentInventory);
+                    currentInventoryTotal+=data.currentInventory;
                     break;
                 // 安全库存量(T)
                 case (6):
                     $(this).html(data.safety);
+                    safetyTotal+=data.safety;
                     break;
                 // 市场采购量
                 case (7):
                     $(this).html(data.marketPurchases);
+                    marketPurchasesTotal+=data.marketPurchases;
                     break;
                 //热值max
                 case (8):
@@ -112,6 +138,7 @@ function setMaterialList(obj,n) {
                        var obj1=eval(item);
                         if (obj1.parameter.name =="热值") {
                             calorificmax=obj1.maximum;
+                           calorificmaxTotal+=obj1.maximum;
                         }
                     });
                     $(this).html(calorificmax);
@@ -122,6 +149,7 @@ function setMaterialList(obj,n) {
                         var obj1 = eval(item);
                         if (obj1.parameter.name =="热值") {
                             calorificmix= obj1.minimum;
+                            calorificminTotal+=obj1.minimum;;
                         }
                     });
                     $(this).html(calorificmix);
@@ -132,6 +160,8 @@ function setMaterialList(obj,n) {
                         var obj1 = eval(item);
                         if (obj1.parameter.name =="灰分") {
                             ascmax= obj1.maximum;
+                            ashmaxTotal+=obj1.maximum;
+
                         }
                     });
                     $(this).html(ascmax);
@@ -142,6 +172,7 @@ function setMaterialList(obj,n) {
                         var obj1 = eval(item);
                         if (obj1.parameter.name =="灰分") {
                             ascmix= obj1.minimum;
+                            ashminTotal+=obj1.minimum;
                         }
                     });
                     $(this).html(ascmix);
@@ -152,6 +183,7 @@ function setMaterialList(obj,n) {
                         var obj1 = eval(item);
                         if (obj1.parameter.name =="含水率") {
                             watermax= obj1.maximum;
+                            watermaxTotal+=obj1.maximum;
                         }
                     });
                     $(this).html(watermax);
@@ -162,6 +194,7 @@ function setMaterialList(obj,n) {
                         var obj1 = eval(item);
                         if (obj1.parameter.name =="含水率") {
                             watermin= obj1.minimum;
+                            waterminTotal+=obj1.minimum;
                         }
                     });
                     $(this).html(watermin);
@@ -172,6 +205,7 @@ function setMaterialList(obj,n) {
                         var obj1 = eval(item);
                         if (obj1.parameter.name =="硫含量") {
                             smax= obj1.maximum;
+                            smaxTotal+=obj1.maximum;
                         }
                     });
                     $(this).html(smax);
@@ -182,6 +216,7 @@ function setMaterialList(obj,n) {
                         var obj1 = eval(item);
                         if (obj1.parameter.name =="硫含量") {
                             smin= obj1.minimum;
+                            sminTotal+=obj1.minimum;
                         }
                     });
                     $(this).html(smin);
@@ -192,6 +227,7 @@ function setMaterialList(obj,n) {
                         var obj1 = eval(item);
                         if (obj1.parameter.name =="氯含量") {
                             clmax= obj1.maximum;
+                            clmaxTotal+=obj1.maximum;
                         }
                     });
                     $(this).html(clmax);
@@ -202,6 +238,7 @@ function setMaterialList(obj,n) {
                         var obj1 = eval(item);
                         if (obj1.parameter.name =="氯含量") {
                             clmin= obj1.minimum;
+                            clminTotal+=obj1.minimum;
                         }
                     });
                     $(this).html(clmin);
@@ -212,6 +249,7 @@ function setMaterialList(obj,n) {
                         var obj1 = eval(item);
                         if (obj1.parameter.name =="磷含量") {
                             pmax= obj1.maximum;
+                            pmaxTotal+=obj1.maximum;
                         }
                     });
                     $(this).html(pmax);
@@ -222,6 +260,7 @@ function setMaterialList(obj,n) {
                         var obj1 = eval(item);
                         if (obj1.parameter.name =="磷含量") {
                             pmin= obj1.minimum;
+                            pminTotal+=obj1.minimum;
                         }
                     });
                     $(this).html(pmin);
@@ -232,6 +271,7 @@ function setMaterialList(obj,n) {
                         var obj1 = eval(item);
                         if (obj1.parameter.name =="氟含量") {
                             fmax= obj1.maximum;
+                            fmaxTotal+=obj1.maximum;
                         }
                     });
                     $(this).html(fmax);
@@ -242,6 +282,7 @@ function setMaterialList(obj,n) {
                         var obj1 = eval(item);
                         if (obj1.parameter.name =="氟含量") {
                             fmin= obj1.minimum;
+                            fminTotal+=obj1.minimum;
                         }
                     });
                     $(this).html(fmin);
@@ -252,6 +293,7 @@ function setMaterialList(obj,n) {
                         var obj1 = eval(item);
                         if (obj1.parameter.name =="酸碱度") {
                             phmax= obj1.maximum;
+                            phmaxTotal+=obj1.maximum;
                         }
                     });
                     $(this).html(phmax);
@@ -262,6 +304,7 @@ function setMaterialList(obj,n) {
                         var obj1 = eval(item);
                         if (obj1.parameter.name =="酸碱度") {
                             phmin= obj1.minimum;
+                            phminTotal+=obj1.minimum;
                         }
                     });
                     $(this).html(phmin);
@@ -285,7 +328,61 @@ function setMaterialList(obj,n) {
         clonedTr.removeAttr("id");
         clonedTr.removeAttr("style");
         clonedTr.insertBefore(tr);
+
     });
     // 隐藏无数据的tr
     tr.hide();
+    //赋值
+    $("#dailyProportionsTotal").text(dailyProportionsTotal);
+    $("#currentInventoryTotal").text(currentInventoryTotal);
+    $("#safetyTotal").text(safetyTotal);
+    $("#marketPurchasesTotal").text(marketPurchasesTotal);
+    $("#calorificmaxTotal").text(calorificmaxTotal);
+    $("#calorificminTotal").text(calorificminTotal);
+    $("#ashmaxTotal").text(ashmaxTotal);
+    $("#ashminTotal").text(ashminTotal);
+    $("#watermaxTotal").text(watermaxTotal);
+    $("#waterminTotal").text(waterminTotal);
+    $("#smaxTotal").text(smaxTotal);
+    $("#sminTotal").text(sminTotal);
+    $("#clmaxTotal").text(clmaxTotal);
+    $("#clminTotal").text(clminTotal);
+    $("#pmaxTotal").text(pmaxTotal);
+    $("#pminTotal").text(pminTotal);
+    $("#fmaxTotal").text(fmaxTotal);
+    $("#fminTotal").text(fminTotal);
+    $("#phmaxTotal").text(phmaxTotal);
+    $("#phminTotal").text(clminTotal);
+}
+/*导入物料需求*/
+function importMaExcel() {
+    document.getElementById("idExcel").click();
+    document.getElementById("idExcel").addEventListener("change", function () {
+        var eFile = document.getElementById("idExcel").files[0];
+        var formFile = new FormData();
+        formFile.append("excelFile", eFile);
+        $.ajax({
+            type: "POST",                       // 方法类型
+            url: "importMaterialRequireIdExcel",              // url
+            async: false,                      // 同步：意思是当有返回值以后才会进行后面的js程序
+            dataType: "json",
+            data: formFile,
+            processData: false,
+            contentType: false,
+            success: function (result) {
+                if (result != undefined) {
+                    console.log(result);
+                    if (result.status == "success") {
+                        alert(result.message);
+                        window.location.reload();         //刷新
+                    } else {
+                        alert(result.message);
+                    }
+                }
+            },
+            error: function (result) {
+                console.log(result);
+            }
+        });
+    });
 }
