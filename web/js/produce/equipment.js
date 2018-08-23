3c66eda900ecbc5a508a10365e6a6b92f29cae92
Leon <18114853285@163.com>
2018-08-23 08:51:56 +0800

    panel重置功能完成

--- web/js/produce/equipment.js
+++ web/js/produce/equipment.js
@@ -4,3 +4,10 @@ function allSelect() {
     if (isChecked) $("input[name='select']").prop('checked',true);
     else $("input[name='select']").prop('checked',false);
 }
+/**
+ * 重置搜索数据
+ */
+function reset() {
+    $("#senior").find("input").val("");
+    $("#senior").find("select").get(0).selectedIndex = -1;
+}
