ccf5c93ce49f3fec643e0285437ddd713b265131
MattJasonLu <1132464757@qq.com>
2018-09-11 18:29:31 +0800

    生产日报逻辑初步V4

--- src/com/jdlink/controller/ProductionDailyController.java
+++ src/com/jdlink/controller/ProductionDailyController.java
@@ -2,10 +2,9 @@ package com.jdlink.controller;
 
 import com.jdlink.domain.Inventory.InboundOrderItem;
 import com.jdlink.domain.Inventory.OutboundOrder;
-import com.jdlink.domain.Produce.HandleCategory;
-import com.jdlink.domain.Produce.MedicalWastes;
-import com.jdlink.domain.Produce.ProductionDaily;
+import com.jdlink.domain.Produce.*;
 import com.jdlink.service.InboundService;
+import com.jdlink.service.IngredientsService;
 import com.jdlink.service.MedicalWastesService;
 import com.jdlink.service.OutboundOrderService;
 import net.sf.json.JSONObject;
@@ -30,6 +29,8 @@ public class ProductionDailyController {
     OutboundOrderService outboundOrderService;
     @Autowired
     MedicalWastesService medicalWastesService;
+    @Autowired
+    IngredientsService ingredientsService;
 
     /**
      * 生成当天日报
@@ -292,6 +293,121 @@ public class ProductionDailyController {
                     productionDaily.getTodayOutboundWastesSludge() + productionDaily.getTodayOutboundWastesDistillation() + productionDaily.getTodayOutboundWastesSuspension() +
                     productionDaily.getTodayOutboundWastesWasteLiquid());
 
+            // 辅料备件消耗
+            // 医疗蒸煮
+            float disposalNaclo = 0f;
+            float disposalDeodorant = 0f;
+            float disposalMedicalWastesBag = 0f;
+            float disposalMedicalPackingPlasticBag = 0f;
+            float disposalCollectionBox = 0f;
+            float disposalSteam = 0f;
+            float disposalIndustrialWater = 0f;
+            float disposalElectricQuantity = 0f;
+            List<Ingredients> ingredientsA2List = ingredientsService.getIngredientsOutItemByRange(now, now, Equipment.MedicalCookingSystem);
+            for (Ingredients ingredients : ingredientsA2List) {
+                switch (ingredients.getName()) {
+                    case "消毒液(NaCLO)":
+                        disposalNaclo += ingredients.getReceiveAmount();
+                        break;
+                    case "除臭剂":
+                        disposalDeodorant += ingredients.getReceiveAmount();
+                        break;
+                    case "医废吨袋":
+                        disposalMedicalWastesBag += ingredients.getReceiveAmount();
+                        break;
+                    case "医废包装塑料袋":
+                        disposalMedicalPackingPlasticBag += ingredients.getReceiveAmount();
+                        break;
+                    case "收集转运箱":
+                        disposalCollectionBox += ingredients.getReceiveAmount();
+                        break;
+                    case "蒸汽":
+                        disposalSteam += ingredients.getReceiveAmount();
+                        break;
+                    case "工业水量":
+                        disposalIndustrialWater += ingredients.getReceiveAmount();
+                        break;
+                    case "电量":
+                        disposalElectricQuantity += ingredients.getReceiveAmount();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            productionDaily.setTodayDisposalMedicalAuxiliaryNaclo(disposalNaclo);
+            productionDaily.setTodayDisposalMedicalAuxiliaryDeodorant(disposalDeodorant);
+            productionDaily.setTodayDisposalMedicalAuxiliaryMedicalWastesBag(disposalMedicalWastesBag);
+            productionDaily.setTodayDisposalMedicalAuxiliaryMedicalPackingPlasticBag(disposalMedicalPackingPlasticBag);
+            productionDaily.setTodayDisposalMedicalAuxiliaryCollectionBox(disposalCollectionBox);
+            productionDaily.setTodayDisposalMedicalAuxiliarySteam(disposalSteam);
+            productionDaily.setTodayDisposalMedicalAuxiliaryIndustrialWater(disposalIndustrialWater);
+            productionDaily.setTodayDisposalMedicalAuxiliaryElectricQuantity(disposalElectricQuantity);
+
+            // 二期
+
+            float disposalCalcareousLime = 0f;
+            float disposalCommonActivatedCarbon = 0f;
+            float disposalActivatedCarbon = 0f;
+            float disposalActivatedCarbonParticles = 0f;
+            float disposalLye = 0f;
+            float disposalSalt = 0f;
+            float disposalSlagBag = 0f;
+            float disposalFlyAshBag = 0f;
+            float disposalDieselOil = 0f;
+            disposalIndustrialWater = 0f;
+            disposalElectricQuantity = 0f;
+            float disposalWoodenPallets = 0f;
+            List<Ingredients> ingredientsSecondaryList = ingredientsService.getIngredientsOutItemByRange(now, now, Equipment.SecondaryTwoCombustionChamber);
+            for (Ingredients ingredients : ingredientsSecondaryList) {
+                switch (ingredients.getName()) {
+                    case "消石灰":
+                        disposalCalcareousLime += ingredients.getReceiveAmount();
+                        break;
+                    case "普通活性炭粉":
+                        disposalCommonActivatedCarbon += ingredients.getReceiveAmount();
+                        break;
+                    case "高活性碳粉":
+                        disposalActivatedCarbon += ingredients.getReceiveAmount();
+                        break;
+                    case "碱液":
+                        disposalLye += ingredients.getReceiveAmount();
+                        break;
+                    case "盐":
+                        disposalSalt += ingredients.getReceiveAmount();
+                        break;
+                    case "炉渣用吨袋":
+                        disposalSlagBag += ingredients.getReceiveAmount();
+                        break;
+                    case "飞灰用吨袋":
+                        disposalFlyAshBag += ingredients.getReceiveAmount();
+                        break;
+                    case "柴油":
+                        disposalDieselOil += ingredients.getReceiveAmount();
+                        break;
+                    case "电量":
+                        disposalElectricQuantity += ingredients.getReceiveAmount();
+                        break;
+                    case "工业水量":
+                        disposalIndustrialWater += ingredients.getReceiveAmount();
+                        break;
+                    case "木托盘":
+                        disposalWoodenPallets += ingredients.getReceiveAmount();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            productionDaily.setTodayDisposalSecondaryAuxiliaryCalcareousLime(disposalCalcareousLime);
+            productionDaily.setTodayDisposalSecondaryAuxiliaryCommonActivatedCarbon(disposalCommonActivatedCarbon);
+            productionDaily.setTodayDisposalSecondaryAuxiliaryActivatedCarbon(disposalActivatedCarbon);
+            productionDaily.setTodayDisposalSecondaryAuxiliaryLye(disposalLye);
+            productionDaily.setTodayDisposalSecondaryAuxiliarySalt(disposalSalt);
+            productionDaily.setTodayDisposalSecondaryAuxiliarySlagBag(disposalSlagBag);
+            productionDaily.setTodayDisposalSecondaryAuxiliaryFlyAshBag(disposalFlyAshBag);
+            productionDaily.setTodayDisposalSecondaryAuxiliaryDieselOil(disposalDieselOil);
+            productionDaily.setTodayDisposalSecondaryAuxiliaryIndustrialWater(disposalIndustrialWater);
+            productionDaily.setTodayDisposalSecondaryAuxiliaryElectricQuantity(disposalElectricQuantity);
+            productionDaily.setTodayDisposalSecondaryAuxiliaryWoodenPallets(disposalWoodenPallets);
 
             // 获取当天的危废入库信息
             List<InboundOrderItem> inboundOrderItemList = inboundService.getInboundOrderItemByRange(now, now);
