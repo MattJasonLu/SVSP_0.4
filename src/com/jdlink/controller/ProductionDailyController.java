3c77c19227a8703b959536bc888cc7fd9807198a
MattJasonLu <1132464757@qq.com>
2018-09-11 16:41:53 +0800

    生产日报逻辑初步V2

--- src/com/jdlink/controller/ProductionDailyController.java
+++ src/com/jdlink/controller/ProductionDailyController.java
@@ -1,10 +1,13 @@
 package com.jdlink.controller;
 
 import com.jdlink.domain.Inventory.InboundOrderItem;
+import com.jdlink.domain.Inventory.OutboundOrder;
+import com.jdlink.domain.Produce.HandleCategory;
 import com.jdlink.domain.Produce.MedicalWastes;
 import com.jdlink.domain.Produce.ProductionDaily;
 import com.jdlink.service.InboundService;
 import com.jdlink.service.MedicalWastesService;
+import com.jdlink.service.OutboundOrderService;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -23,6 +26,8 @@ public class ProductionDailyController {
 
     @Autowired
     InboundService inboundService;
+    @Autowired
+    OutboundOrderService outboundOrderService;
     @Autowired
     MedicalWastesService medicalWastesService;
 
@@ -79,6 +84,195 @@ public class ProductionDailyController {
             productionDaily.setTodayOutboundMedicalWastesDirectDisposal(productionDaily.getTodayDisposalMedicalWastesDisposalDirect());
             productionDaily.setTodayOutboundMedicalWastesCooking(productionDaily.getTodayInboundMedicalWastesCooking()+productionDaily.getTodayInboundMedicalWastesErrorNumber());
             productionDaily.setTodayOutboundMedicalWastesAfterCooking(productionDaily.getTodayDisposalMedicalWastesAfterCooking());
+
+            // 工废处置系统
+            float disposalBulk = 0f;
+            float disposalCrushing = 0f;
+            float disposalSludge = 0f;
+            float disposalDistillation = 0f;
+            float disposalSuspension = 0f;
+            float disposalWasteLiquid = 0f;
+            float disposalMedicalWastes = 0f;
+            List<OutboundOrder> outboundA2OrderList = outboundOrderService.getOutBoundByDateAndEquipment(now, "A2");
+            for (OutboundOrder outboundOrder : outboundA2OrderList) {
+                HandleCategory handelCategory = outboundOrder.getHandelCategory();
+                switch (handelCategory) {
+                    case Bulk:
+                        disposalBulk += outboundOrder.getOutboundNumber();
+                        break;
+                    case Crushing:
+                        disposalCrushing += outboundOrder.getOutboundNumber();
+                        break;
+                    case Sludge:
+                        disposalSludge += outboundOrder.getOutboundNumber();
+                        break;
+                    case Distillation:
+                        disposalDistillation += outboundOrder.getOutboundNumber();
+                        break;
+                    case Suspension:
+                        disposalSuspension += outboundOrder.getOutboundNumber();
+                        break;
+                    case WasteLiquid:
+                        disposalWasteLiquid += outboundOrder.getOutboundNumber();
+                        break;
+                }
+            }
+
+            productionDaily.setTodayOutboundA2WastesBulk(disposalBulk);
+            productionDaily.setTodayOutboundA2WastesCrushing(disposalCrushing);
+            productionDaily.setTodayOutboundA2WastesSludge(disposalSludge);
+            productionDaily.setTodayOutboundA2WastesDistillation(disposalDistillation);
+            productionDaily.setTodayOutboundA2WastesSuspension(disposalSuspension);
+            productionDaily.setTodayOutboundA2WastesWasteLiquid(disposalWasteLiquid);
+            productionDaily.setTodayOutboundA2MedicalWastes(disposalMedicalWastes);
+
+            // 备2
+            disposalBulk = 0f;
+            disposalCrushing = 0f;
+            disposalSludge = 0f;
+            disposalDistillation = 0f;
+            disposalSuspension = 0f;
+            disposalWasteLiquid = 0f;
+            disposalMedicalWastes = 0f;
+
+            List<OutboundOrder> outboundPrepare2OrderList = outboundOrderService.getOutBoundByDateAndEquipment(now, "Prepare2");
+            for (OutboundOrder outboundOrder : outboundPrepare2OrderList) {
+                HandleCategory handelCategory = outboundOrder.getHandelCategory();
+                switch (handelCategory) {
+                    case Bulk:
+                        disposalBulk += outboundOrder.getOutboundNumber();
+                        break;
+                    case Crushing:
+                        disposalCrushing += outboundOrder.getOutboundNumber();
+                        break;
+                    case Sludge:
+                        disposalSludge += outboundOrder.getOutboundNumber();
+                        break;
+                    case Distillation:
+                        disposalDistillation += outboundOrder.getOutboundNumber();
+                        break;
+                    case Suspension:
+                        disposalSuspension += outboundOrder.getOutboundNumber();
+                        break;
+                    case WasteLiquid:
+                        disposalWasteLiquid += outboundOrder.getOutboundNumber();
+                        break;
+                }
+            }
+
+            productionDaily.setTodayOutboundPrepare2WastesBulk(disposalBulk);
+            productionDaily.setTodayOutboundPrepare2WastesCrushing(disposalCrushing);
+            productionDaily.setTodayOutboundPrepare2WastesSludge(disposalSludge);
+            productionDaily.setTodayOutboundPrepare2WastesDistillation(disposalDistillation);
+            productionDaily.setTodayOutboundPrepare2WastesSuspension(disposalSuspension);
+            productionDaily.setTodayOutboundPrepare2WastesWasteLiquid(disposalWasteLiquid);
+            productionDaily.setTodayOutboundPrepare2MedicalWastes(disposalMedicalWastes);
+
+            // B2
+            disposalBulk = 0f;
+            disposalCrushing = 0f;
+            disposalSludge = 0f;
+            disposalDistillation = 0f;
+            disposalSuspension = 0f;
+            disposalWasteLiquid = 0f;
+            disposalMedicalWastes = 0f;
+
+            List<OutboundOrder> outboundB2OrderList = outboundOrderService.getOutBoundByDateAndEquipment(now, "B2");
+            for (OutboundOrder outboundOrder : outboundB2OrderList) {
+                HandleCategory handelCategory = outboundOrder.getHandelCategory();
+                switch (handelCategory) {
+                    case Bulk:
+                        disposalBulk += outboundOrder.getOutboundNumber();
+                        break;
+                    case Crushing:
+                        disposalCrushing += outboundOrder.getOutboundNumber();
+                        break;
+                    case Sludge:
+                        disposalSludge += outboundOrder.getOutboundNumber();
+                        break;
+                    case Distillation:
+                        disposalDistillation += outboundOrder.getOutboundNumber();
+                        break;
+                    case Suspension:
+                        disposalSuspension += outboundOrder.getOutboundNumber();
+                        break;
+                    case WasteLiquid:
+                        disposalWasteLiquid += outboundOrder.getOutboundNumber();
+                        break;
+                }
+            }
+
+            productionDaily.setTodayOutboundB2WastesBulk(disposalBulk);
+            productionDaily.setTodayOutboundB2WastesCrushing(disposalCrushing);
+            productionDaily.setTodayOutboundB2WastesSludge(disposalSludge);
+            productionDaily.setTodayOutboundB2WastesDistillation(disposalDistillation);
+            productionDaily.setTodayOutboundB2WastesSuspension(disposalSuspension);
+            productionDaily.setTodayOutboundB2WastesWasteLiquid(disposalWasteLiquid);
+            productionDaily.setTodayOutboundB2MedicalWastes(disposalMedicalWastes);
+
+            // 三期
+            disposalBulk = 0f;
+            disposalCrushing = 0f;
+            disposalSludge = 0f;
+            disposalDistillation = 0f;
+            disposalSuspension = 0f;
+            disposalWasteLiquid = 0f;
+            disposalMedicalWastes = 0f;
+
+            List<OutboundOrder> outboundThirdOrderList = outboundOrderService.getOutBoundByDateAndEquipment(now, "ThirdPhasePretreatmentSystem");
+            for (OutboundOrder outboundOrder : outboundThirdOrderList) {
+                HandleCategory handelCategory = outboundOrder.getHandelCategory();
+                switch (handelCategory) {
+                    case Bulk:
+                        disposalBulk += outboundOrder.getOutboundNumber();
+                        break;
+                    case Crushing:
+                        disposalCrushing += outboundOrder.getOutboundNumber();
+                        break;
+                    case Sludge:
+                        disposalSludge += outboundOrder.getOutboundNumber();
+                        break;
+                    case Distillation:
+                        disposalDistillation += outboundOrder.getOutboundNumber();
+                        break;
+                    case Suspension:
+                        disposalSuspension += outboundOrder.getOutboundNumber();
+                        break;
+                    case WasteLiquid:
+                        disposalWasteLiquid += outboundOrder.getOutboundNumber();
+                        break;
+                }
+            }
+
+            productionDaily.setTodayOutboundThirdPretreatmentSystemWastesBulk(disposalBulk);
+            productionDaily.setTodayOutboundThirdPretreatmentSystemWastesCrushing(disposalCrushing);
+            productionDaily.setTodayOutboundThirdPretreatmentSystemWastesSludge(disposalSludge);
+            productionDaily.setTodayOutboundThirdPretreatmentSystemWastesDistillation(disposalDistillation);
+            productionDaily.setTodayOutboundThirdPretreatmentSystemWastesSuspension(disposalSuspension);
+            productionDaily.setTodayOutboundThirdPretreatmentSystemWastesWasteLiquid(disposalWasteLiquid);
+            productionDaily.setTodayOutboundThirdPretreatmentSystemMedicalWastes(disposalMedicalWastes);
+
+
+            // 危废处置量
+            productionDaily.setTodayOutboundWastesBulk(productionDaily.getTodayOutboundA2WastesBulk() +
+                    productionDaily.getTodayOutboundPrepare2WastesBulk() + productionDaily.getTodayOutboundB2WastesBulk() +
+                    productionDaily.getTodayOutboundThirdPretreatmentSystemWastesBulk());
+            productionDaily.setTodayOutboundWastesCrushing(productionDaily.getTodayOutboundA2WastesCrushing() +
+                    productionDaily.getTodayOutboundPrepare2WastesCrushing() + productionDaily.getTodayOutboundB2WastesCrushing() +
+                    productionDaily.getTodayOutboundThirdPretreatmentSystemWastesCrushing());
+            productionDaily.setTodayOutboundWastesSludge(productionDaily.getTodayOutboundA2WastesSludge() +
+                    productionDaily.getTodayOutboundPrepare2WastesSludge() + productionDaily.getTodayOutboundB2WastesSludge() +
+                    productionDaily.getTodayOutboundThirdPretreatmentSystemWastesSludge());
+            productionDaily.setTodayOutboundWastesDistillation(productionDaily.getTodayOutboundA2WastesDistillation() +
+                    productionDaily.getTodayOutboundPrepare2WastesDistillation() + productionDaily.getTodayOutboundB2RateWastesDistillation() +
+                    productionDaily.getTodayOutboundThirdPretreatmentSystemWastesDistillation());
+            productionDaily.setTodayOutboundWastesSuspension(productionDaily.getTodayOutboundA2WastesSuspension() + productionDaily.getTodayOutboundB2WastesSuspension() +
+                    productionDaily.getTodayOutboundPrepare2WastesSuspension() + productionDaily.getTodayOutboundThirdPretreatmentSystemWastesSuspension());
+            productionDaily.setTodayOutboundWastesWasteLiquid(productionDaily.getTodayOutboundA2WastesWasteLiquid() + productionDaily.getTodayOutboundB2WastesWasteLiquid() +
+                    productionDaily.getTodayOutboundPrepare2WastesWasteLiquid() + productionDaily.getTodayOutboundThirdPretreatmentSystemWastesWasteLiquid());
+            productionDaily.setTodayOutboundWastesTotal(productionDaily.getTodayOutboundWastesBulk() + productionDaily.getTodayOutboundWastesCrushing() +
+                    productionDaily.getTodayOutboundWastesSludge() + productionDaily.getTodayOutboundWastesDistillation() + productionDaily.getTodayOutboundWastesSuspension() +
+                    productionDaily.getTodayOutboundWastesWasteLiquid());
 
             // 获取当天的危废入库信息
             List<InboundOrderItem> inboundOrderItemList = inboundService.getInboundOrderItemByRange(now, now);
