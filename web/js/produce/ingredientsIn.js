b15a765099f63d821fcd7f63aeb611761c285aa2
wuhanxue <wuhanxue5@sina.com>
2018-09-21 09:02:32 +0800

    辅料备件回车查询

--- web/js/produce/ingredientsIn.js
+++ web/js/produce/ingredientsIn.js
@@ -514,6 +514,15 @@ function importInExcel() {
 }
 
 /**
+ * 回车查询
+ */
+function enterSearch(){
+    if (event.keyCode === 13) {   // 如果按下键为回车键，即执行搜素
+        searchIngredientIn();      //
+    }
+}
+
+/**
  * 查询功能
  */
 function searchIngredientIn() {
