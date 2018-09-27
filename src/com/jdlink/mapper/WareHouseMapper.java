f4c29e539e1968c05652e901cc5d9b0c212083a1
MattJsaon <1132464757@qq.com>
2018-09-27 15:46:50 +0800

    危废导入导出功能完成

---
+++ src/com/jdlink/mapper/WareHouseMapper.java
@@ -0,0 +1,38 @@
+package com.jdlink.mapper;
+
+import com.jdlink.domain.Inventory.WareHouse;
+
+public interface WareHouseMapper {
+
+    /**
+     * 获取编号
+     * @return 编号
+     */
+    String getCurrentId();
+    /**
+     * 增加仓库
+     * @param wareHouse 仓库
+     */
+    void add(WareHouse wareHouse);
+
+    /**
+     * 通过仓库名找到仓库
+     * @param name 仓库名
+     */
+    WareHouse getWareHouseByName(String name);
+
+    /**
+     * 通过编号获取到仓库
+     * @param id 编号
+     * @return 仓库
+     */
+    WareHouse getWareHouseById(String id);
+
+    /**
+     * 获取仓库的数量
+     * @return 仓库的数量
+     */
+    int count();
+
+
+}
