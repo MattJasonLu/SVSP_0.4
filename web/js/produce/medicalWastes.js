b712c1ceb7980d23c8429535b9700196a041639e
JackYang950618 <1169267380@qq.com>
2018-10-11 17:38:19 +0800

    医疗入库导入完成

--- web/js/produce/medicalWastes.js
+++ web/js/produce/medicalWastes.js
@@ -789,3 +789,109 @@ function cancelMedicalWastes(item) {
 
 }
 
+
+//导入数据
+function importExcelChoose() {
+    $("#importExcelModal").modal('show');
+}
+
+function importExcel() {
+    document.getElementById("idExcel").click();
+    document.getElementById("idExcel").addEventListener("change", function () {
+        var eFile = document.getElementById("idExcel").files[0];
+        var formFile = new FormData();
+        formFile.append("excelFile", eFile);
+        $.ajax({
+            type: "POST",                       // 方法类型
+            url: "importMedicalWaste",              // url
+            async: false,                      // 同步：意思是当有返回值以后才会进行后面的js程序
+            dataType: "json",
+            data: formFile,
+            processData: false,
+            contentType: false,
+            success: function (result) {
+                if (result != undefined) {
+                    console.log(result);
+                    if (result.status == "success") {
+
+                    } else {
+                        alert(result.message);
+                    }
+                }
+            },
+            error: function (result) {
+                console.log(result);
+            }
+        });
+        alert("导入成功！");
+        window.location.reload();         //刷新
+    });
+}
+
+// function importExcel() {
+//     document.getElementById("idExcel").click();
+//     document.getElementById("idExcel").addEventListener("change", function () {
+//         var id = '0000';
+//         console.log("change");
+//         $.ajax({
+//             type: "POST",                       // 方法类型
+//             url: "getCurrentCompatibilityId",              // url
+//             async: false,                      // 同步：意思是当有返回值以后才会进行后面的js程序
+//             dataType: "json",
+//             contentType: false,
+//             success: function (result) {
+//                 if (result != undefined || result != NaN) {
+//                     id = result.compatibilityId;
+//                 } else {
+//                     alert("数据获取失败！ " + result);
+//                 }
+//             },
+//             error: function (result) {
+//                 alert("导入失败，请检查后重试！")
+//                 console.log("error" + result);
+//             }
+//         });
+//         var eFile = document.getElementById("idExcel").files[0];
+//         var formFile = new FormData();
+//         formFile.append("excelFile", eFile);
+//         formFile.append("tableName", 't_pr_pw');
+//         formFile.append("id", id);
+//         $.ajax({
+//             type: "POST",                       // 方法类型
+//             url: "importCompatibilityExcel",              // url
+//             async: false,                      // 同步：意思是当有返回值以后才会进行后面的js程序
+//             dataType: "json",
+//             data: formFile,
+//             processData: false,
+//             contentType: false,
+//             success: function (result) {
+//                 if (result != undefined) {
+//                     console.log(result);
+//                     if (result.status == "success") {
+//                         alert(result.message);
+//                         window.location.reload();         //刷新
+//                     } else {
+//                         alert(result.message);
+//                     }
+//                 }
+//             },
+//             error: function (result) {
+//                 console.log(result);
+//             }
+//         });
+//     });
+//
+// }
+
+/**
+ * 下载模板
+ * */
+function downloadModal() {
+    var filePath = 'Files/Templates/配伍周导入模板.xlsx';
+    var r = confirm("是否下载模板?");
+    if (r == true) {
+        window.open('downloadFile?filePath=' + filePath);
+    }
+}
+
+
