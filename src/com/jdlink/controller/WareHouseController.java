470b094a1011f0ac9ee0f4c637cf6f88fac7375a
MattJsaon <1132464757@qq.com>
2018-12-25 14:22:50 +0800

    仓库管理完成

--- src/com/jdlink/controller/WareHouseController.java
+++ src/com/jdlink/controller/WareHouseController.java
@@ -6,6 +6,7 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -56,6 +57,7 @@ public class WareHouseController {
         JSONObject res=new JSONObject();
         try {
             WareHouse wareHouse = wareHouseService.getWareHouseById(id);
+            if (wareHouse == null) throw new NullPointerException("未找到数据");
             JSONObject data = JSONObject.fromBean(wareHouse);
             res.put("status", "success");
             res.put("message", "获取成功");
@@ -75,7 +77,7 @@ public class WareHouseController {
      */
     @RequestMapping("updateWareHouse")
     @ResponseBody
-    public String updateWareHouse(WareHouse wareHouse) {
+    public String updateWareHouse(@RequestBody WareHouse wareHouse) {
         JSONObject res=new JSONObject();
         try {
             wareHouseService.update(wareHouse);
@@ -95,6 +97,7 @@ public class WareHouseController {
      * @return 成功与否
      */
     @RequestMapping("deleteWareHouseById")
+    @ResponseBody
     public String deleteWareHouseById(String id) {
         JSONObject res=new JSONObject();
         try {
@@ -105,6 +108,28 @@ public class WareHouseController {
             e.printStackTrace();
             res.put("status", "fail");
             res.put("message", "删除失败");
+        }
+        return res.toString();
+    }
+
+    /**
+     * 增加仓库
+     * @param wareHouse 仓库对象
+     * @return 成功与否
+     */
+    @RequestMapping("addWareHouse")
+    @ResponseBody
+    public String addWareHouse(@RequestBody WareHouse wareHouse) {
+        JSONObject res = new JSONObject();
+        try {
+            // 增加仓库对象
+            wareHouseService.add(wareHouse);
+            res.put("status", "success");
+            res.put("message", "新增成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            res.put("status", "fail");
+            res.put("message", "新增失败");
         }
         return res.toString();
     }
