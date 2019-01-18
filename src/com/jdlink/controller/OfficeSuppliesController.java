d7f9763bf9d4e279434ecf0004092b724ca7ae19
MattLuJason <1132464757@qq.com>
2019-01-18 11:34:36 +0800

    办公用品管理后端完成

--- src/com/jdlink/controller/OfficeSuppliesController.java
+++ src/com/jdlink/controller/OfficeSuppliesController.java
@@ -1,17 +1,29 @@
 package com.jdlink.controller;
 
+import com.jdlink.domain.OfficeSuppliesInbound;
 import com.jdlink.domain.OfficeSuppliesItem;
+import com.jdlink.domain.OfficeSuppliesOutbound;
+import com.jdlink.service.OfficeSuppliesService;
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * 办公用品控制器
  */
 @Controller
 public class OfficeSuppliesController {
+
+    @Autowired
+    OfficeSuppliesService officeSuppliesService;
+
+    /*******************************************************入库部分********************************************************/
 
     /**
      * 列出所有入库单数据
@@ -22,9 +34,11 @@ public class OfficeSuppliesController {
     public String listOfficeSuppliesInbound(@RequestBody OfficeSuppliesItem officeSuppliesItem) {
         JSONObject res = new JSONObject();
         try {
-
+            List<OfficeSuppliesInbound> officeSuppliesInboundList = officeSuppliesService.listOfficeSuppliesInbound(officeSuppliesItem);
+            JSONArray data = JSONArray.fromArray(officeSuppliesInboundList.toArray(new OfficeSuppliesInbound[officeSuppliesInboundList.size()]));
             res.put("status", "success");
             res.put("message", "获取数据成功");
+            res.put("data", data);
         } catch (Exception e) {
             e.printStackTrace();
             res.put("status", "fail");
@@ -32,6 +46,33 @@ public class OfficeSuppliesController {
         }
         return res.toString();
     }
+
+    /**
+     * 增加办公用品入库单
+     * @param officeSuppliesInbound 办公用品入库单
+     * @return 成功与否
+     */
+    @RequestMapping("addOfficeSuppliesInbound")
+    @ResponseBody
+    public String addOfficeSuppliesInbound(@RequestBody OfficeSuppliesInbound officeSuppliesInbound) {
+        JSONObject res = new JSONObject();
+        try {
+            officeSuppliesService.addOfficeSuppliesInbound(officeSuppliesInbound);
+            res.put("status", "success");
+            res.put("message", "新增成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            res.put("status", "fail");
+            res.put("message", "新增失败");
+        }
+        return res.toString();
+    }
+
+    /*******************************************************入库部分********************************************************/
+
+
+
+    /*******************************************************出库部分********************************************************/
 
     /**
      * 列出所有出库单数据
@@ -42,9 +83,11 @@ public class OfficeSuppliesController {
     public String listOfficeSuppliesOutbound(@RequestBody OfficeSuppliesItem officeSuppliesItem) {
         JSONObject res = new JSONObject();
         try {
-
+            List<OfficeSuppliesOutbound> officeSuppliesOutboundList = officeSuppliesService.listOfficeSuppliesOutbound(officeSuppliesItem);
+            JSONArray data = JSONArray.fromArray(officeSuppliesOutboundList.toArray(new OfficeSuppliesOutbound[officeSuppliesOutboundList.size()]));
             res.put("status", "success");
             res.put("message", "获取数据成功");
+            res.put("data", data);
         } catch (Exception e) {
             e.printStackTrace();
             res.put("status", "fail");
@@ -52,5 +95,28 @@ public class OfficeSuppliesController {
         }
         return res.toString();
     }
+
+    /**
+     * 增加办公用品出库单
+     * @param officeSuppliesOutbound 办公用品出库单
+     * @return 成功与否
+     */
+    @RequestMapping("addOfficeSuppliesOutbound")
+    @ResponseBody
+    public String addOfficeSuppliesOutbound(@RequestBody OfficeSuppliesOutbound officeSuppliesOutbound) {
+        JSONObject res = new JSONObject();
+        try {
+            officeSuppliesService.addOfficeSuppliesOutbound(officeSuppliesOutbound);
+            res.put("status", "success");
+            res.put("message", "新增成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            res.put("status", "fail");
+            res.put("message", "新增失败");
+        }
+        return res.toString();
+    }
+
+    /*******************************************************出库部分********************************************************/
 
 }
--- src/com/jdlink/domain/OfficeSuppliesInbound.java
+++ src/com/jdlink/domain/OfficeSuppliesInbound.java
@@ -1,5 +1,6 @@
 package com.jdlink.domain;
 
+import com.jdlink.domain.Dictionary.CheckStateItem;
 import com.jdlink.domain.Dictionary.TicketRateItem;
 import com.jdlink.domain.Dictionary.UnitDataItem;
 
@@ -27,11 +28,71 @@ public class OfficeSuppliesInbound {
      */
     private Float totalPrice;
     /**
-     * 出库时间
+     * 入库时间
      */
-    private Date outboundDate;
+    private Date inboundDate;
     /**
      * 创建时间
      */
     private Date createTime;
+    /**
+     * 审批状态
+     */
+    private CheckStateItem checkStateItem;
+
+    public String getInboundId() {
+        return inboundId;
+    }
+
+    public void setInboundId(String inboundId) {
+        this.inboundId = inboundId;
+    }
+
+    public List<OfficeSuppliesItem> getOfficeSuppliesItemList() {
+        return officeSuppliesItemList;
+    }
+
+    public void setOfficeSuppliesItemList(List<OfficeSuppliesItem> officeSuppliesItemList) {
+        this.officeSuppliesItemList = officeSuppliesItemList;
+    }
+
+    public Float getTotalTaxPrice() {
+        return totalTaxPrice;
+    }
+
+    public void setTotalTaxPrice(Float totalTaxPrice) {
+        this.totalTaxPrice = totalTaxPrice;
+    }
+
+    public Float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Date getInboundDate() {
+        return inboundDate;
+    }
+
+    public void setInboundDate(Date inboundDate) {
+        this.inboundDate = inboundDate;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public CheckStateItem getCheckStateItem() {
+        return checkStateItem;
+    }
+
+    public void setCheckStateItem(CheckStateItem checkStateItem) {
+        this.checkStateItem = checkStateItem;
+    }
 }
