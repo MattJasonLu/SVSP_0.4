a13151ada1fa16f0c743422b6536fb81054f021d
wuhanxue <wuhanxue5@sina.com>
2019-03-07 15:07:25 +0800

    审批流数据结构调整

--- web/js/produce/approvalManage.js
+++ web/js/produce/approvalManage.js
@@ -9,23 +9,19 @@ function showEditModal() {
 }
 
 function addNewLine(item) {
-
-    // 获取id为plusBtn的tr元素
-    //var tr = $("#plusBtn").prev();
+    // 获取上一行tr
     var tr = $(item).parent().parent().prev();
     // 克隆tr，每次遍历都可以产生新的tr
     var clonedTr = tr.clone();
-    $(clonedTr).children('td').eq(0).find('p').hide()
+    $(clonedTr).children('td').eq(0).find('p').hide();
     clonedTr.attr('class', 'myclass3');
     clonedTr.show();
+    // 清空旧数据
     clonedTr.children().find("input").val("");
+    clonedTr.children().find("select").val("");
     var delBtn = "<a class='btn btn-default btn-xs' onclick='delLine(this);'><span class='glyphicon glyphicon-minus' aria-hidden='true'></span></a>";
-
-
     clonedTr.children('td').eq(0).find("a").remove();
-    clonedTr.children('td').eq(0).append(delBtn)
-
-
+    clonedTr.children('td').eq(0).append(delBtn);   // 增加减行按钮
     clonedTr.insertAfter(tr);
     clonedTr.removeAttr("id");
 }
